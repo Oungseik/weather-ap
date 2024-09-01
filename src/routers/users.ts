720bfe762c40bbaf87c9235cad1cbd89daaa44e6
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-09-01 07:32:20 +0630

    feat: add note and look up in admin panel

--- src/routers/users.ts
+++ src/routers/users.ts
@@ -2,7 +2,11 @@ import { Hono } from "hono";
 
 import { User } from "@/utils/mongodb.js";
 
-export const router = new Hono();
+type Variables = {
+  user: Omit<User, "password">;
+};
+
+export const router = new Hono<{ Variables: Variables }>();
 
 router.get("/count", async (c) => {
   const count = await User.countDocuments({ role: "USER" });
@@ -10,7 +14,29 @@ router.get("/count", async (c) => {
 });
 
 router.get("/", async (c) => {
-  let users = await User.find({}, { projection: { password: 0 } }).toArray();
+  const users = await User.find({}, { projection: { password: 0 } }).toArray();
 
   return c.json(users);
+});
+
+router.get("/one", async (c) => {
+  const { username } = c.get("user");
+  const user = await User.findOne({ username: username }, { projection: { password: 0 } });
+  return c.json(user);
+});
+
+router.post("/", async (c) => {
+  const user = c.get("user");
+
+  const { transportation, note } = await c.req.json();
+  await User.updateOne(
+    { username: user.username },
+    {
+      $set: {
+        transportation,
+        note,
+      },
+    }
+  );
+  return c.json({ username: user.username });
 });
