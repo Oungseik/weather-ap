f4c0897aa0dcdc922f72c6de774b9ffaba836af4
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-09-02 14:04:52 +0630

    feat: use cloud mongodb

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,9 @@
 export const config = {
-  mongoURL: "mongodb://localhost:27017",
+  mongoURL:
+    "mongodb+srv://honey:CXTPKYxPHFcgdWAz@mylearningcluster.6lct3wo.mongodb.net/?retryWrites=true&w=majority&appName=MyLearningCluster",
   db: "weather-app",
   port: 5000,
   secret: "a delicious cookie secret",
 };
+
+//CXTPKYxPHFcgdWAz
